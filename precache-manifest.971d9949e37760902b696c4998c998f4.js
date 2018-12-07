c757960fd36a0f8c0ebe148b57bffd68d4cf9bb5
ZPPD <zhana.p.d@gmail.com>
2018-12-07 13:09:12 -0500

    Updates

---
+++ precache-manifest.971d9949e37760902b696c4998c998f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa7078e144624bd434e6555c6e7da217",
+    "url": "/MovieApp/static/media/theatre1.aa7078e1.jpg"
+  },
+  {
+    "revision": "bdcb4cd8aa9e21f95428ef816ca83823",
+    "url": "/MovieApp/static/media/popcorn.bdcb4cd8.jpg"
+  },
+  {
+    "revision": "5dfb1f54ad9e9ba2849e",
+    "url": "/MovieApp/static/js/runtime~main.5dfb1f54.js"
+  },
+  {
+    "revision": "23887734aebc6275d1ab",
+    "url": "/MovieApp/static/js/main.23887734.chunk.js"
+  },
+  {
+    "revision": "eb00bc654ebe295fe158",
+    "url": "/MovieApp/static/js/1.eb00bc65.chunk.js"
+  },
+  {
+    "revision": "23887734aebc6275d1ab",
+    "url": "/MovieApp/static/css/main.e78a2b39.chunk.css"
+  },
+  {
+    "revision": "fa649dcac36afabdf1c88d422c941fde",
+    "url": "/MovieApp/index.html"
+  }
+];
